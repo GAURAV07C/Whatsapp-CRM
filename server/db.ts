78468220e49bf62c539950768910cfb3c8ec566a
Gaurav Kumar <gaurav989267@gmail.com>
2026-01-30 12:52:40 +0530

    fixed

--- server/db.ts
+++ server/db.ts
@@ -9,21 +9,13 @@ import * as schema from "@shared/schema";
 
 async function initDB() {
   // MySQL connection using URL
-<<<<<<< HEAD
   const connectionUrl = process.env.MYSQL_URL!
-=======
-  let connectionUrl = process.env.MYSQL_URL!
-  
-  // Remove ssl-mode parameter as mysql2 doesn't support it in URL
-  // and add proper SSL config
-  connectionUrl = connectionUrl.replace(/[?&]ssl-mode=[^&]*/gi, '');
->>>>>>> 1fbea6a6a24b8c64fb69d71e6b7da89d3b68fc9c
 
   const connection = await mysql.createConnection({
     uri: connectionUrl,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    // ssl: {
+    //   rejectUnauthorized: false
+    // }
   });
 
   console.log("✅ Database connected successfully!");
